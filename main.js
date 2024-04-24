9d7d429722d4039ba067574c6b6900e138a98625
Dhanushs3103 <dhanushvictor2003@gmail.com>
2024-04-24 10:28:46 +0530

    Completed the code of Country APP with sort funtionality

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 let root = document.querySelector("#root");
 let button = document.querySelector("#fetchBtn");
+let sortingData = document.querySelector("#sortingData");
+let countryData = null;
 
+// EventListener for fetching data
 button.addEventListener("click", getData);
 
+// EventListener for sorting data
+sortingData.addEventListener("change", handleSortChange);
+
+// Function to handle sort change event
+function handleSortChange() {
+    let selectedOption = sortingData.value;
+    if (selectedOption === "desc") {
+        sortDesc();
+    } else if (selectedOption === "asc") {
+        sortAsec();
+    }
+}
 async function getData() {
 
-let res = await fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries`);
-let countryData = await res.json();
-console.log(countryData.data);
-displayData(countryData.data);
-
+    if (!countryData) { 
+        let res = await fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries`);
+        countryData = await res.json();
+        console.log(countryData.data);
+        displayData(countryData.data);
+    }
 }
 
 function displayData(countries) {
+    root.innerHTML = "";
     countries.forEach((country) => {
         // card creation
         let card = document.createElement("div");
@@ -42,11 +59,16 @@ function displayData(countries) {
     
 }
 
-function sortDesc () {
-
+async function sortDesc () {
+    let res = await fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries?sort=population&order=desc`);
+    let countryData = await res.json();
+    displayData(countryData.data);
 
 }
 
-function sortAsc () {
+async function sortAsec () {
+    let res = await fetch(`https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-countries?sort=population&order=asc`);
+    let countryData = await res.json();
+    displayData(countryData.data);
 
 }
